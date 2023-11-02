af22c5ab4f9a7f7b6c30f383505a72a4a7c61cc8
Mangayarkarasi J <mangayarkarasi.j@caratlane.com>
2023-11-02 09:46:04 +0000

    Another groupby query

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -3,5 +3,5 @@
 
 {{ config(materialized='table') }}
 
-select count(*) from public.segment
-where updated_by = 'Ajay Kumar'
+select updated_by, count(*) as cnt from public.segment
+group by updated_by
